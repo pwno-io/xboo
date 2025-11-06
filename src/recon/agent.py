bd7d18c4bc3236fc1d8f217028e10da6dc35782d
retr0reg <retr0@retr0.blog>
2025-11-05 21:36:54 -0500

    Refactor agent initialization in Recon and Scout classes to include empty string prompts and updated response formats. Added tools to Scout agent and implemented message handling in invoke method.

--- src/recon/agent.py
+++ src/recon/agent.py
@@ -4,11 +4,12 @@ from langgraph.prebuilt import create_react_agent
 class Recon:
     def __init__(self):
         def _prompting(state: dict) -> str:
-            return 
+            return ""
         self.model = ChatOpenAI(model="gpt-5")
         self.agent = create_react_agent(
             self.model,
-            prompt=_prompting
+            prompt=_prompting,
+            response_format=None, # NOTE: I recommend just using state.py's typings
         )
 
     def invoke(self, state):
--- src/scout/agent.py
+++ src/scout/agent.py
@@ -1,16 +1,26 @@
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage, SystemMessage
+from .tool import execution, python
 # create agent
 
 class Scout:
     def __init__(self):
         def _prompting(state: dict) -> str:
-            return 
+            return "" # system prompt
         self.model = ChatOpenAI(model="gpt-5")
         self.agent = create_react_agent(
             self.model,
-            prompt=_prompting
+            prompt=_prompting,
+            response_format=None, # NOTE: I recommend just using state.py's typings
+            tools=[execution, python]
         )
 
     def invoke(self, state):
-        pass
+        result: None = self.agent.invoke( 
+            {
+                "messages": [
+                    HumanMessage(content=state["message"])
+                ]
+            }
+        )
